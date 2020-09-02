3cbe347acfd442187d5b1fab58967a1da7d35678
pvarouktsis <p.varouktsis@gmail.com>
2020-09-02 03:22:45 +0300

    Implement 2D matrix for transitionTimes of orders

--- algo/src/main/java/com/oma/algo/Main.java
+++ algo/src/main/java/com/oma/algo/Main.java
@@ -3,14 +3,28 @@ package com.oma.algo;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
-      ArrayList<Order> orders = new ArrayList<>();
-      OrderGenerator og = new OrderGenerator();
-      orders = og.generateOrders();
-  
-      for (Order order: orders)
-        System.out.println(order.toString());
-    }
+  private static final int SEED = 19092000;
+  private static final int MAX_ORDERS = 100;
+
+  public static void main(String[] args) {
+    ArrayList<Order> orders = new ArrayList<>();
+    double[][] transitionTimes = new double[MAX_ORDERS][MAX_ORDERS];
+    OrderGenerator og = new OrderGenerator(SEED, MAX_ORDERS);
+
+    orders = og.generateOrders();
+    transitionTimes = og.generateTransitionTimesOfOrders();
+
+    // print orders
+    // (id, quantity, dark)
+    for (Order order: orders)
+      System.out.println(order.toString());
+
+    // print transitionTimes
+    // (i, j, transitionTimes[i][j])
+    for (int i = 0; i < MAX_ORDERS; i++)
+      for (int j = 0; j < MAX_ORDERS; j++)
+        System.out.println("(" + i + ", " + j + ", " + transitionTimes[i][j] + ")");
+  }
 
 }
 
--- algo/src/main/java/com/oma/algo/OrderGenerator.java
+++ algo/src/main/java/com/oma/algo/OrderGenerator.java
@@ -4,24 +4,47 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class OrderGenerator {
-  private static final int SEED = 19092000;
-  private static final int MAX_ORDERS = 100;
+  private int seed = 19092000;
+  private int maxOrders = 100;
+  private Random rg = new Random(seed);
+
+  public OrderGenerator(int seed, int maxOrders) {
+    this.seed = seed;
+    this.maxOrders = maxOrders;
+  }
 
   public ArrayList<Order> generateOrders() {
     ArrayList<Order> orders = new ArrayList<>();
-    Random rg = new Random(SEED);
 
-    for (int i = 0; i < MAX_ORDERS; i++) {
+    for (int i = 0; i < maxOrders; i++) {
+      int id = i + 1;
       int quantity = 100 + rg.nextInt(401);
       boolean dark = false;
       if (rg.nextDouble() < 0.15) {
         dark = true;
       }
-      Order o = new Order(i+1, quantity, dark);
+      Order o = new Order(id, quantity, dark);
       orders.add(o);
     }
 
     return orders;
+  }
+
+  public double[][] generateTransitionTimesOfOrders() {
+    double[][] transitionTimes = new double[maxOrders][maxOrders];
+    
+    for (int i = 0; i < maxOrders; i++) {
+      for (int j = 0; j < maxOrders; j++) {
+        double time = 0;
+        double random = rg.nextDouble();
+        if (i == j) 
+          continue;
+        time = Math.round((10 + 20 * random) * 100.0) / 100.0;
+        transitionTimes[i][j] = time;
+      }
+    }
+
+    return transitionTimes;
   }
 
 }
