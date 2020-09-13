79e9dbc2623e4e0bb84e837579cba4588ad77de3
Christos Pappas <chrispappas99@yahoo.gr>
2020-09-13 17:17:34 +0300

    Create main function skeleton
    Algorithms are still unimplemented

--- algo/src/main/java/com/oma/algo/Main.java
+++ algo/src/main/java/com/oma/algo/Main.java
@@ -4,7 +4,17 @@ public class Main {
 
   public static void main(String[] args) {
     AssemblyLine colorFactory = new AssemblyLine();
-    Solution sol = colorFactory.createInitialSolution();
+    Solution sln = colorFactory.createInitialSolution();
+    Algorithm algo1 = new StrategicAlgorithm();
+    sln = algo1.compute(sln);
+    Algorithm algo2 = new SteepestDescentAlgorithm();
+    sln = algo2.compute(sln);
+    Algorithm algo3 = new VNDAlgorithm();
+    sln = algo3.compute(sln);
+    printSolution(sln);
+  }
+
+  public static void printSolution(Solution sln) {
+    System.out.println("Hello world");
   }
 }
-
