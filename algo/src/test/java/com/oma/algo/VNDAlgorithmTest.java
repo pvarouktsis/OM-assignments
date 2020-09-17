7bc392c40ce4d014db53678e6644e3184a84a327
Christos Pappas <chrispappas99@yahoo.gr>
2020-09-18 01:08:50 +0300

    Create testing class for VNDAlgorithm

---
+++ algo/src/test/java/com/oma/algo/VNDAlgorithmTest.java
@@ -0,0 +1,11 @@
+package com.oma.algo;
+
+import org.junit.*;
+
+public class VNDAlgorithmTest {
+
+    @Test
+    public void testCompute() {
+        Assert.assertTrue("Hello tests!", true);
+    }
+}
