b41d0965ea9117d4051a22ac545142c0ace2aaf2
Christos Pappas <chrispappas99@yahoo.gr>
2020-09-20 17:54:19 +0300

    Improve Move Types

--- algo/src/main/java/com/oma/algo/InsertionMoveType.java
+++ algo/src/main/java/com/oma/algo/InsertionMoveType.java
@@ -13,6 +13,9 @@ public class InsertionMoveType implements MoveType {
     @Override
     public boolean improveSolution(Solution sol) {
         double bestTime = assemblyLine.calculateTime(sol);
+        List<Order> machineToRemove = null, machineToAdd = null;
+        boolean improved = false;
+        int idxToRemove = 0, idxToAdd = 0;
         for (List<Order> currentMachine : sol) {
             for (int i = 0; i < currentMachine.size(); i++) {
                 Order chosenOrder = currentMachine.remove(i);
@@ -22,15 +25,25 @@ public class InsertionMoveType implements MoveType {
                             continue; // Don't put it in the same position it was before
                         }
                         targetMachine.add(j, chosenOrder);
-                        double swappedTime = assemblyLine.calculateTime(sol);
-                        if (swappedTime < bestTime) {
-                            return true; // Found improvement, we are done
+                        double newTime = assemblyLine.calculateTime(sol);
+                        if (newTime < bestTime) {
+                            bestTime = newTime;
+                            improved = true;
+                            machineToRemove = currentMachine;
+                            machineToAdd = targetMachine;
+                            idxToRemove = i;
+                            idxToAdd = j;
                         }
                         targetMachine.remove(j); // Reset the solution to previous state
                     }
                 }
                 currentMachine.add(i, chosenOrder);
             }
+        }
+        if (improved) {
+            Order chosenOrder = machineToRemove.remove(idxToRemove);
+            machineToAdd.add(idxToAdd, chosenOrder);
+            return true;
         }
         return false;
     }
--- algo/src/main/java/com/oma/algo/SwapMoveType.java
+++ algo/src/main/java/com/oma/algo/SwapMoveType.java
@@ -14,6 +14,9 @@ public class SwapMoveType implements MoveType {
     @Override
     public boolean improveSolution(Solution sol) {
         double bestTime = assemblyLine.calculateTime(sol);
+        boolean improved = false;
+        List<Order> bestMachine1 = null, bestMachine2 = null;
+        int bestIdx1 = -1, bestIdx2 = -1;
         for (List<Order> currentMachine : sol) {
             for (int i = 0; i < currentMachine.size(); i++) {
                 Iterator<List<Order>> targetIt = sol.iterator();
@@ -27,12 +30,21 @@ public class SwapMoveType implements MoveType {
                         swapElements(currentMachine, i, targetMachine, j);
                         double swappedTime = assemblyLine.calculateTime(sol);
                         if (swappedTime < bestTime) {
-                            return true; // Found improvement, we are done
+                            bestTime = swappedTime; // Found improvement, we are done
+                            bestMachine1 = currentMachine;
+                            bestMachine2 = targetMachine;
+                            bestIdx1 = i;
+                            bestIdx2 = j;
+                            improved = true;
                         }
                         swapElements(currentMachine, i, targetMachine, j); // Reset the solution to previous state
                     }
                 }
             }
+        }
+        if (improved) {
+            swapElements(bestMachine1, bestIdx1, bestMachine2, bestIdx2);
+            return true;
         }
         return false;
     }
