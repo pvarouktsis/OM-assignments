f1896a125da17976d5bccf22071f4be3d219eadd
pvarouktsis <p.varouktsis@gmail.com>
2020-09-20 18:44:14 +0300

    Fix roll move type
    Roll move type now returns when it searches all possible solutions and
    finds the best of them.

--- algo/src/main/java/com/oma/algo/RollMoveType.java
+++ algo/src/main/java/com/oma/algo/RollMoveType.java
@@ -11,19 +11,28 @@ public class RollMoveType implements MoveType {
 
     public boolean improveSolution(Solution s) {
         double btime = assemblyLine.calculateTime(s);
+        int bcount = -1;
 
         for (int c = 0; c < assemblyLine.orders.size(); c++) {
-            s = rollOrders(s);
+            s = rollForwardOrders(s);
             double time = assemblyLine.calculateTime(s);
             if (btime > time) {
-                return true;
+                btime = time;
+                bcount = c;
             }
+        }
+
+        if (bcount != -1) {
+            for (int c = assemblyLine.orders.size() - 1; c > bcount; c--) {
+                s = rollBackOrders(s);
+            }
+            return true;
         }
 
         return false;
     }
 
-    public Solution rollOrders(Solution s) {
+    public Solution rollForwardOrders(Solution s) {
         Order prevOrder = null;
 
         for (List<Order> machine : s) {
@@ -37,6 +46,24 @@ public class RollMoveType implements MoveType {
         List<Order> machine = s.iterator().next();
         machine.add(0, prevOrder);
         
+        return s;
+    }
+
+    public Solution rollBackOrders(Solution s) {
+        Order firstOrder = null;
+        List<Order> prevMachine = null;
+
+        for (List<Order> machine : s) {
+            if (firstOrder == null) {
+                firstOrder = machine.remove(0);
+                prevMachine = machine;
+                continue;
+            }
+            prevMachine.add(machine.remove(0));
+            prevMachine = machine;
+        }
+        prevMachine.add(firstOrder);
+
         return s;
     }
 }
